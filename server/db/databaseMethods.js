6c029d3f407949e3a6322edbfbb5cba1df9ec2fb
Phillip Korolog <phillipkorolog@phillips-mbp.lan>
2020-03-30 17:51:58 -0600

    database methods now generating the correct structure, just need to add to db

--- server/db/databaseMethods.js
+++ server/db/databaseMethods.js
@@ -1,18 +1,17 @@
 const mysql = require('mysql');
 
 
-const insertIntoTeams = function (dataArray) {
-    console.log("data array", dataArray);
+const insertIntoTeams = function (dataObj) {
     var queryString = "INSERT INTO teams (id, teamName, city, abbreviation, divisionName, divisionID) VALUES ?";
     var values = [];
-    for (var x = 0; x < dataArray.length; x++ ) {
+    for (var x = 1; x < dataObj.teams.length; x++ ) {
         var tempArray = [];
-        tempArray.push(dataArray[x].id);
-        tempArray.push(dataArray[x].name);
-        tempArray.push(dataArray[x].venue.city);
-        tempArray.push(dataArray[x].abbreviation);
-        tempArray.push(dataArray[x].division.name);
-        tempArray.push(dataArray[x].division.id);
+        tempArray.push(dataObj.teams[x].id);  
+        tempArray.push(dataObj.teams[x].name);
+        tempArray.push(dataObj.teams[x].venue.city);
+        tempArray.push(dataObj.teams[x].abbreviation);
+        tempArray.push(dataObj.teams[x].division.name);
+        tempArray.push(dataObj.teams[x].division.id);
         values.push(tempArray);
     }
     console.log("values", values);
